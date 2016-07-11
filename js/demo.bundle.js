e287658e7075ac874b6d446a2742f835adcae68d
Adam Ullman <adam@tokbox.com>
2016-07-11 18:23:57 +1000

    Pointing to the glasses properly

--- js/demo.bundle.js
+++ js/demo.bundle.js
@@ -241,7 +241,8 @@
 	        tmpCanvas.width = canvas.width;
 	        tmpCanvas.height = canvas.height;
 	        image = document.createElement('img');
-	        image.src = imageSrc || '/images/comedy-glasses.png';
+	        image.src = imageSrc ||
+	          'https://aullman.github.io/opentok-camera-filters/images/comedy-glasses.png';
 	      }
 	      tmpCtx.putImageData(imgData, 0, 0);
 	
