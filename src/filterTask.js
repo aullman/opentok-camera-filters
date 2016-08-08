1f64c7950c817c90d267517116f378f41ac43f6a
Adam Ullman <adam@tokbox.com>
2016-08-08 14:49:41 +1000

    Remove accidental logging

--- src/filterTask.js
+++ src/filterTask.js
@@ -3,7 +3,6 @@ module.exports = function filterTask(videoElement, canvas, selectedFilter) {
   let tmpCtx;
   let ctx;
   let stopped = false;
-  let myId = Math.random() * 1000;
 
   // Draws a frame on the specified canvas after applying the selected filter every
   // requestAnimationFrame
@@ -22,7 +21,6 @@ module.exports = function filterTask(videoElement, canvas, selectedFilter) {
     const data = selectedFilter(imgData);
     ctx.putImageData(data, 0, 0);
     if (!stopped) {
-      console.log(`requestAnimationFrame ${myId}`);
       requestAnimationFrame(drawFrame);
     } else {
       tmpCanvas = null;
