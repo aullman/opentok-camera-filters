1a6a8fbc13ceaab34a565f6e7070a9c43949bf4a
Adam Ullman <adam@tokbox.com>
2016-08-26 14:27:53 +1000

    Generate a new token every time

--- demo/src/index.js
+++ demo/src/index.js
@@ -1,4 +1,4 @@
-/* global OT */
+/* global OT config */
 // A real app would use require('opentok-filters/src/filters.js');
 const filters = require('../../src/filters.js');
 // A real app would use require('opentok-filters')(filters.none);
@@ -21,15 +21,14 @@ selector.addEventListener('change', () => {
 // Wait for OT to load before we start using it
 window.addEventListener('load', () => {
   // Simple Hello World App
-  const session = OT.initSession('1127',
-    '2_MX4xMTI3fn4xNDcyMTcxNjQxMDc2fkpNc1N0cDRCdzIwQXdidkhGVEZickpDMX5-');
+  const session = OT.initSession(config.OT_API_KEY, config.OT_SESSION_ID);
   session.on('streamCreated', event => {
     session.subscribe(event.stream, err => {
       if (err) alert(err.message);
     });
   });
 
-  session.connect('T1==cGFydG5lcl9pZD0xMTI3JnNpZz1mMzAyMTc0N2NjMTA3YjZkZWYzOGI1Y2VmOGI0OWM2MjFlMWQ4YWM0OnNlc3Npb25faWQ9Ml9NWDR4TVRJM2ZuNHhORGN5TVRjeE5qUXhNRGMyZmtwTmMxTjBjRFJDZHpJd1FYZGlka2hHVkVaaWNrcERNWDUtJmNyZWF0ZV90aW1lPTE0NzIxNzE2NDEmbm9uY2U9MC41OTQzMTAwNDI5ODc2Mjg3JnJvbGU9bW9kZXJhdG9yJmV4cGlyZV90aW1lPTE0NzQ3NjM2NDE=', err => {
+  session.connect(config.OT_TOKEN, err => {
     if (err) alert(err.message);
     const publisher = session.publish(null, {
       resolution: '320x240',
--- demo/webpack.config.js
+++ demo/webpack.config.js
@@ -1,3 +1,7 @@
+const webpack = require('webpack');
+const OpenTok = require('opentok');
+const opentok = new OpenTok(process.env.OT_API_KEY, process.env.OT_API_SECRET);
+
 module.exports = {
   entry: {
     index: './src/index.js',
@@ -8,4 +12,13 @@ module.exports = {
     filename: '[name].bundle.js',
   },
   devtool: 'source-map',
+  plugins: [
+    new webpack.DefinePlugin({
+      config: {
+        OT_API_KEY: JSON.stringify(process.env.OT_API_KEY),
+        OT_SESSION_ID: JSON.stringify(process.env.OT_SESSION_ID),
+        OT_TOKEN: JSON.stringify(opentok.generateToken(process.env.OT_SESSION_ID)),
+      },
+    }),
+  ],
 };
