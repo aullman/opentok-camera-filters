ab3a1987a290080d70cc194e077db52b2f75deef
Adam Ullman <adam@tokbox.com>
2016-08-26 14:29:32 +1000

    Use a 2 day expire time

--- demo/webpack.config.js
+++ demo/webpack.config.js
@@ -17,7 +17,9 @@ module.exports = {
       config: {
         OT_API_KEY: JSON.stringify(process.env.OT_API_KEY),
         OT_SESSION_ID: JSON.stringify(process.env.OT_SESSION_ID),
-        OT_TOKEN: JSON.stringify(opentok.generateToken(process.env.OT_SESSION_ID)),
+        OT_TOKEN: JSON.stringify(opentok.generateToken(process.env.OT_SESSION_ID, {
+          expireTime: (new Date().getTime() / 1000) + (2 * 24 * 60 * 60), // in 2 days
+        })),
       },
     }),
   ],
