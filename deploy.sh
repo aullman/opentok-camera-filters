40691cc3e8c5062a4fc7d35217a8cb6207814d6e
Adam Ullman <adam@tokbox.com>
2016-12-09 14:50:35 +1100

    Accidentally left out this line

--- deploy.sh
+++ deploy.sh
@@ -16,10 +16,10 @@ function doCompile {
 }
 
 # Pull requests and commits to other branches shouldn't try to deploy, just build to verify
-# if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" -o "$BROWSER" != "chrome" -o "$BVER" != "stable" ]; then
-#     echo "Skipping deploy."
-#     exit 0
-# fi
+if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" -o "$BROWSER" != "chrome" -o "$BVER" != "stable" ]; then
+    echo "Skipping deploy."
+    exit 0
+fi
 
 # Save some useful information
 REPO=`git config remote.origin.url`
