1eb47e8a3f2c7205f306e99dda4fbbb3f1f72e85
Adam Ullman <adam@tokbox.com>
2017-09-18 17:13:07 +1000

    Get it working in Safari on iOS! 📱🎉
    It seems like Safari on iOS doesn't like setting the srcObject of a VideoElement to be a Canvas.captureStream. So instead I'm just putting the Canvas on top of the video element in the Publisher. Also, Safari on iOS doesn't like 320x240.

--- src/filter.js
+++ src/filter.js
@@ -10,26 +10,29 @@ let initialFilter;
 // Mock out getUserMedia and replace the stream with the canvas.captureStream()
 mockGetUserMedia(stream => {
   videoElement = document.createElement('video');
-  videoElement.muted = 'true';
   videoElement.srcObject = stream;
+  videoElement.setAttribute('playsinline', '');
+  videoElement.muted = true;
+  setTimeout(() => {
+    videoElement.play();
+  });
+  const canvasStream = canvas.captureStream();
 
   videoElement.addEventListener('loadedmetadata', () => {
-    videoElement.play();
     canvas.width = videoElement.videoWidth;
     canvas.height = videoElement.videoHeight;
     videoElementLoaded = true;
+
     if (initialFilter) {
       selectedFilter = initialFilter(videoElement, canvas);
     }
+    if (stream.getAudioTracks().length) {
+      // Add the audio track to the stream
+      // This actually doesn't work in Firefox until version 49
+      // https://bugzilla.mozilla.org/show_bug.cgi?id=1271669
+      canvasStream.addTrack(stream.getAudioTracks()[0]);
+    }
   });
-
-  const canvasStream = canvas.captureStream();
-  if (stream.getAudioTracks().length) {
-    // Add the audio track to the stream
-    // This actually doesn't work in Firefox until version 49
-    // https://bugzilla.mozilla.org/show_bug.cgi?id=1271669
-    canvasStream.addTrack(stream.getAudioTracks()[0]);
-  }
   return canvasStream;
 });
 
@@ -44,7 +47,20 @@ module.exports = iFilter => {
       // We insert the canvas into the publisher element. captureStream() only works
       // with Canvas elements that are visible and in the DOM.
       const pubEl = document.querySelector(`#${publisher.id}`);
-      pubEl.appendChild(canvas);
+      const widgetContainer = pubEl.querySelector('.OT_widget-container');
+      let pubVid = pubEl.querySelector('.OT_widget-container video');
+      widgetContainer.insertBefore(canvas, pubVid);
+      canvas.style.width = '100%';
+      canvas.style.height = '100%';
+      const setObjectFit = () => {
+        pubVid = pubEl.querySelector('.OT_widget-container video');
+        canvas.style.objectFit = window.getComputedStyle(pubVid).objectFit;
+      };
+      if (!pubVid) {
+        publisher.on('videoElementCreated', setObjectFit);
+      } else {
+        setObjectFit();
+      }
       publisher.on('destroyed', () => {
         // Stop running the filter
         if (selectedFilter) {
