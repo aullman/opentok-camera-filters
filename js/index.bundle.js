8509e3993ec5a44df0bed90732fb24348b769e3c
Travis CI <adam@tokbox.com>
2017-10-04 01:20:33 +0000

    Deploy to GitHub Pages: 97cdd0f895e6f70fafe611e560a325e6a980a7d9

--- js/index.bundle.js
+++ js/index.bundle.js
@@ -74,7 +74,7 @@
 	    });
 	  });
 	
-	  session.connect(("T1==cGFydG5lcl9pZD00NDkzNTM0MSZzaWc9YjNlNWVjNjkzYTI0MGQ3OTc3YzRhNDU2NGRhZTJhNTJmMDFlODA1ODpzZXNzaW9uX2lkPTFfTVg0ME5Ea3pOVE0wTVg1LU1UUTJPRGd3T0RZMk5qUXhPSDU2TldkR1FrOU9TaTl3S3l0NVlWcHFiREpVVG5aT1YyWi1mZyZjcmVhdGVfdGltZT0xNTA2NDc0OTgwJm5vbmNlPTAuMTk2NzIxNjQ5MjYzMDU0MTMmcm9sZT1wdWJsaXNoZXImZXhwaXJlX3RpbWU9MTUwNzMzODk4MCZpbml0aWFsX2xheW91dF9jbGFzc19saXN0PQ=="), err => {
+	  session.connect(("T1==cGFydG5lcl9pZD00NDkzNTM0MSZzaWc9MDAwM2Q3N2EwOWQ1YjBlZDhiNDhmMzFmYjdkNThmZjRkNzkyYzA4MzpzZXNzaW9uX2lkPTFfTVg0ME5Ea3pOVE0wTVg1LU1UUTJPRGd3T0RZMk5qUXhPSDU2TldkR1FrOU9TaTl3S3l0NVlWcHFiREpVVG5aT1YyWi1mZyZjcmVhdGVfdGltZT0xNTA3MDgwMDMxJm5vbmNlPTAuODU5MzI3MDg0NjE3Njg5MyZyb2xlPXB1Ymxpc2hlciZleHBpcmVfdGltZT0xNTA3OTQ0MDMxJmluaXRpYWxfbGF5b3V0X2NsYXNzX2xpc3Q9"), err => {
 	    if (err) alert(err.message);
 	    const publisher = session.publish(null);
 	    filter.setPublisher(publisher);
