2332d5b3fffe764e38c4003ffd1b9e43bd76a178
Adam Ullman <adam@tokbox.com>
2018-01-22 15:49:49 +1100

    Using the official way to do custom streams with v2.13

--- demo/src/index.js
+++ demo/src/index.js
@@ -2,7 +2,8 @@
 // A real app would use require('opentok-filters/src/filters.js');
 const filters = require('../../src/filters.js');
 // A real app would use require('opentok-filters')(filters.none);
-const filter = require('../..')(filters.none);
+const filterFn = require('../..');
+let filter;
 
 const selector = document.querySelector('select');
 let f;
@@ -17,20 +18,35 @@ selector.addEventListener('change', () => {
   filter.change(filters[selector.value]);
 });
 
+const handleError = (err) => {
+  if (err) alert(err.message);
+};
 
-// Wait for OT to load before we start using it
-window.addEventListener('load', () => {
-  // Simple Hello World App
-  const session = OT.initSession(config.OT_API_KEY, config.OT_SESSION_ID);
-  session.on('streamCreated', event => {
-    session.subscribe(event.stream, err => {
-      if (err) alert(err.message);
-    });
-  });
+// Returns a Promise to a Publisher
+const publish = OT.getUserMedia().then((mediaStream) => {
+  filter = filterFn(mediaStream, filters.none);
 
-  session.connect(config.OT_TOKEN, err => {
-    if (err) alert(err.message);
-    const publisher = session.publish(null);
-    filter.setPublisher(publisher);
+  const publisherOptions = {
+    // Pass in the canvas stream video track as our custom videoSource
+    videoSource: filter.canvas.captureStream(30).getVideoTracks()[0],
+    // Pass in the audio track from our underlying mediaStream as the audioSource
+    audioSource: mediaStream.getAudioTracks()[0],
+  };
+
+  const publisher = OT.initPublisher('publisher', publisherOptions, exports.handleError);
+  filter.setPublisher(publisher);
+
+  return publisher;
+});
+
+const session = OT.initSession(config.OT_API_KEY, config.OT_SESSION_ID);
+session.on('streamCreated', event => {
+  session.subscribe(event.stream, handleError);
+});
+
+session.connect(config.OT_TOKEN, err => {
+  if (err) handleError(err);
+  publish.then(publisher => {
+    session.publish(publisher, handleError);
   });
 });
