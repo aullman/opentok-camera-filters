b71a71df2805c66fc2c5d92b074e7d4490bc4bc7
Travis CI <adam@tokbox.com>
2018-12-05 01:48:33 +0000

    Deploy to GitHub Pages: 6830c37a58e270f49a14f831ce4c49f42ae6561b

--- js/index.bundle.js
+++ js/index.bundle.js
@@ -96,7 +96,7 @@
 	  session.subscribe(event.stream, handleError);
 	});
 	
-	session.connect(("T1==cGFydG5lcl9pZD00NDkzNTM0MSZzaWc9NDg3YjM4ZmMwYzdhYWFjM2FmZTk2NGQyOGE4ODUyYzQ4YzkwZWI0MzpzZXNzaW9uX2lkPTFfTVg0ME5Ea3pOVE0wTVg1LU1UUTJPRGd3T0RZMk5qUXhPSDU2TldkR1FrOU9TaTl3S3l0NVlWcHFiREpVVG5aT1YyWi1mZyZjcmVhdGVfdGltZT0xNTQzMzY5Njg5Jm5vbmNlPTAuODQwMjcxMzU1MzUxNDMzMiZyb2xlPXB1Ymxpc2hlciZleHBpcmVfdGltZT0xNTQ0MjMzNjg5JmluaXRpYWxfbGF5b3V0X2NsYXNzX2xpc3Q9"), err => {
+	session.connect(("T1==cGFydG5lcl9pZD00NDkzNTM0MSZzaWc9NDY5NmE4NjAzZTgyN2YxYWMzYjc2Mzg0MmI3MDEwODBkMWJhZGZjNjpzZXNzaW9uX2lkPTFfTVg0ME5Ea3pOVE0wTVg1LU1UUTJPRGd3T0RZMk5qUXhPSDU2TldkR1FrOU9TaTl3S3l0NVlWcHFiREpVVG5aT1YyWi1mZyZjcmVhdGVfdGltZT0xNTQzOTc0NTExJm5vbmNlPTAuNTQwODIwODQ1ODY4NDM4NSZyb2xlPXB1Ymxpc2hlciZleHBpcmVfdGltZT0xNTQ0ODM4NTExJmluaXRpYWxfbGF5b3V0X2NsYXNzX2xpc3Q9"), err => {
 	  if (err) handleError(err);
 	  publish.then(publisher => {
 	    session.publish(publisher, handleError);
